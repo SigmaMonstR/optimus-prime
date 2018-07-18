db2fa0fbd7626fd2034ede97862d7ed0f533ead4
Max Sergent <maxrgnt@Maxs-MacBook-Air.local>
2018-07-18 17:33:41 -0400

    added search bar, still not functional

--- js/populate.js
+++ js/populate.js
@@ -1,11 +1,22 @@
 // external js: isotope.pkgd.js
+var qsRegex;
 
 var $grid = $('.grid').isotope({
   itemSelector: '.grid-item',
   masonry: {
     columnWidth: 80
+  },
+
+  filter: function() {
+    return qsRegex ? $(this).text().match( qsRegex ) : true;
   }
+
 });
+
+var $quicksearch = $('.quicksearch').keyup( debounce ( function() {
+  qsRegex = new RegExp ( $quicksearch.val(), 'gi');
+  $grid.isotope();
+}, 200 ) );
 
 $(window).load(function(){
     
@@ -16,7 +27,6 @@ $(window).load(function(){
           var $items = "<div class='grid-item grid-item--width3 grid-item--height2'><h3>" + bea[i].industry +"</h3>"+
         "<p>GDP Growth:</p>"+bea[i].gdp_growth+"</div>";
         
-          
           // append elements to container
           $grid.append( $items )
             // add and lay out newly appended elements
